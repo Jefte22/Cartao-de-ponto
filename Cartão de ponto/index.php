ff370503e6d1d5bcde4878e93b0fc9088060887c
Jefte22 <106981741+Jefte22@users.noreply.github.com>
2022-09-21 17:03:20 -0300

    Add files via upload

---
+++ Cartão de ponto/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Teste Spaut</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<link href="style.css" rel="stylesheet">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><link type="text/css" rel="stylesheet" href="resources/sheet.css" >
+</head>
+<body>
+	<div class="row container margin0">
+		<div class="col s2 borderA"></div>
+		<div class="col s10 cnpj">
+			<h2 class="titleS">Secretaria de administração</h2>
+			<p>CNPJ: 36.862.621/0001-21</p>
+		</div>
+		<div class="col s12 border">
+			<h2 class="tituloP">Cartão de Ponto</h2>
+		</div>
+		<div class="col s12 border">
+			<p class="descP">CARTÃO DE ACORDO COM ARTIGO 15 DA PORTARIA MTB NRO. 3.626 DE 13/11/1991.</p>
+		</div>
+	</div>
+	<div class="row container border">
+		<div class="col s2 imgP borderR">
+		<img src="download.jpg">	
+		</div>
+		<div class="col s8 content">
+			<p>Periodo:<b>17/17/17</b> a <b>17/17/17</b>   <span class="chapa">Chapa: <b>961384</b><span></p>
+			<p>Nome: <b>Nome Da pessoa</b></p>
+			<p>Quadro: <b>08:00 12:00 14:00 18:00 (SAB/DOM) M</b></p>
+			<p>Departamento: <b>SECRETARIA DE INFRAESTRUTURA E SERVIÇOS</b> </p>
+		</div>
+		<div class="col s2 admissao">
+			<p class="adm"> Data de admissão no sistema<p>16/04/2021 
+		</div>
+	</div>
+	<div class="row">
+		<div class="col s12 form center">
+			<iframe class="iframe-res" frameborder="0" scrolling="false" src="https://onedrive.live.com/embed?resid=C9A19E0524A3A2BF%21630&authkey=%21AO90k5A8L-oz9RM&em=2&wdAllowInteractivity=False&AllowTyping=True&ActiveCell='Planilha1'!A1&Item='Planilha1'!A1%3AI42&wdHideGridlines=True&wdDownloadButton=True&wdInConfigurator=false&wdInConfigurator=False&edesNext=false&resen=true"></iframe>
+	</div>
+</div>
+<div class="row container">
+	<div class="col s12 cnt"><p>Reconheço os registros acima inseridos através da assinatura eletrônica e de minhas fotos</p></div>
+	<div class="col m6 s12 sf"><div class="sign">Nome do Ativo</div></div>
+		<div class="col m6 s12 assinatura">
+			wew748w5e41w85
+			<p>Assinatura Eletrônica</p>
+		</div>
+</div>
+<div class="row container foooter">
+	<div class="col s9">
+		<p class="FooterP">SPAUT Consultoria e Sistemas LTDA. </p></div>
+	<div class="col s3">
+		<p class="FooterP2">(62) 3945-3758</p> </div>
+</div>
+</body>
